fd7718ee1676146b8f83bae2514d09de889c702a
Tatjam <tatjam02@gmail.com>
2022-09-10 23:07:40 +0200

    Switch to nigui, seems to be a better library

--- src/gui/base.nim
+++ src/gui/base.nim
@@ -1,5 +1,58 @@
-let MARGINS* = 8.0
-let STATUS_SIZE* = 28.0
-let BUTTONS_SIZE* = 32.0
+import nigui
+
+import notebooks_view
+
+let MARGINS* = 8
+let STATUS_SIZE* = 28
+let BUTTONS_SIZE* = 32
+
 
 var selected_preset*: string
+
+
+app.init()
+
+var win = newWindow("Remarkable Nim")
+
+win.width = 800.scaleToDpi
+win.height = 600.scaleToDpi
+
+# The window always shows the status bar and a layout container for the other stuff
+var container = newLayoutContainer(Layout_Vertical)
+win.add(container)
+
+var main_container = newLayoutContainer(Layout_Vertical)
+container.add(main_container)
+
+
+var status_container = newLayoutContainer(Layout_Vertical)
+container.add(status_container)
+status_container.yAlign = YAlign_Bottom
+status_container.heightMode = HeightMode_Expand
+status_container.xAlign = XAlign_Center
+status_container.spacing = -18
+
+var status_label = newLabel("Standby")
+status_container.add(status_label)
+# Make sure the progress bar is visible behind the text
+status_label.backgroundColor = rgb(0, 0, 0, 0)
+
+
+var progbar = newProgressBar()
+status_container.add(progbar)
+
+# Dummy container to allow deletion without order change
+var dummy_container = newLayoutContainer(Layout_Vertical)
+main_container.add(dummy_container)
+
+proc goto_view(view: string, meta: string = "") = 
+    main_container.remove(dummy_container)
+    case view:
+    of "notebooks": dummy_container = load_notebooks_view(meta)
+    else: echo "Invalid view"
+    main_container.add(dummy_container)
+
+goto_view("notebooks")
+
+win.show()
+app.run()
--- src/gui/notebooks_view.nim
+++ src/gui/notebooks_view.nim
@@ -1,34 +1,8 @@
-import nimx/[layout,button, text_field]
-import sugar
-import base
-import dropdown_inverted
+import nigui
 
-var goto_presets* = false
+proc load_notebooks_view*(meta: string): LayoutContainer =
+    let cont = newLayoutContainer(Layout_Vertical)
+    let btn = newButton("Hello!")
+    cont.add(btn)
 
-
-proc load_notebooks_view*(into: View, change: (string, string) -> void) = 
-    into.makeLayout:
-        - Button as multi_button:
-            trailing == super.trailing - MARGINS
-            leading == super.trailing - MARGINS - 150.0
-            top == super.bottom - BUTTONS_SIZE - MARGINS
-            bottom == super.bottom - MARGINS
-            title: "Multi"
-        - Button as sync_button:
-            trailing == prev.leading - MARGINS 
-            leading == prev.leading - MARGINS - 150.0
-            top == prev.top
-            bottom == prev.bottom
-            title: "Sync Settings"
-        # The majority of the view is the browser 
-        # Below we have the preset chooser popup button
-        - DropdownInverted as preset_chooser:
-            left == super.leading + MARGINS
-            right == prev.leading - MARGINS
-            top == prev.top
-            bottom == prev.bottom
-            items: ["Hello, world", "Goodbye, world"]
-            onAction:
-                change("preset_select", "")
-    
-
+    return cont
